d2f39c51455ed8fcaba2de514d80c241f813caf6
aniket691 <aniketdalal126@gmail.com>
2025-10-04 00:00:40 +0530

    fjhbf

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap(): Promise<void> {
     const port = parseInt(process.env.PORT ?? '3000', 10);
     const host = process.env.HOST || '0.0.0.0';
 
-    await app.listen(`maze-backend.railway.internal`, '3000');
+    await app.listen(3000, '0.0.0.0');
 
     const displayHost = host === '0.0.0.0' ? 'localhost' : host;
     console.log(`Application is running on: http://localhost:3000`);

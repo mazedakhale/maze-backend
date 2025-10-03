ed324937151ed15ff82108c739afec10d6e00247
aniket691 <aniketdalal126@gmail.com>
2025-10-04 00:04:52 +0530

    dhbdhj

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
@@ -7,19 +9,54 @@ async function bootstrap(): Promise<void> {
   try {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    app.enableCors();
+    app.enableCors({
+      origin: [
+        'http://localhost:5173', // local frontend
+        'https://euphonious-bombolone-63bf12.netlify.app', // deployed frontend
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: 'Content-Type, Authorization',
+    });
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    );
 
     app.useStaticAssets(join(__dirname, '..', 'uploads'), {
       prefix: '/uploads/',
     });
 
+    // Swagger configuration start
+    const config = new DocumentBuilder()
+      .setTitle('Your API Title')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'Enter JWT token to authorize requests',
+        },
+        'jwt-auth', // This name can be anything you want
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    // Swagger configuration end
+
     const port = parseInt(process.env.PORT ?? '3000', 10);
     const host = process.env.HOST || '0.0.0.0';
 
-    await app.listen(3000, '0.0.0.0');
+    await app.listen('3000', '0.0.0.0');
 
     const displayHost = host === '0.0.0.0' ? 'localhost' : host;
-    console.log(`Application is running on: http://localhost:3000`);
+    console.log(`Application is running on: http://${displayHost}:${port}`);
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
